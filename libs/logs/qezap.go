a93f63fbd2be5f1fd9ca0c3738f48dbee303a786
胡忠庆 <zhongqing2016@qq.com>
2021-01-25 15:00:05 +0800

    update qezap package

--- libs/logs/qezap.go
+++ libs/logs/qezap.go
@@ -7,7 +7,22 @@ import (
 
 var Qezap *qezap.Logger
 
+func init() {
+	// 注册一个本地的 Log
+	cfg := qezap.NewConfig(nil, "")
+	Qezap = qezap.New(cfg, zap.DebugLevel)
+}
+
 func InitQezap(addrs []string, moduleName string) {
+	// 默认注册的
+	if Qezap != nil {
+		// 则追加远程模块
+		if len(addrs) > 0 && moduleName != "" {
+			cfg := Qezap.Config().SetEnableRemote(true).SetAddr(addrs).SetModule(moduleName)
+			Qezap.AppendWriter(qezap.NewWriteRemote(cfg))
+		}
+		return
+	}
 	cfg := qezap.NewConfig(addrs, moduleName)
 	if len(addrs) == 0 {
 		cfg.SetEnableRemote(false)
